7cb4266b952668bebdbc60ad59205017c27d22d0
m <marius00-gh@evilsoft.net>
2022-05-25 14:40:50 -0300

    Famous "initial commit": Global keyboard hook with support for logitech G-keys

---
+++ Logitech/LogitechGKeys.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net.Mime;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+class LogitechGKeys {
+    /*
+     LogiGkeyGetKeyboardGkeyString
+LogiGkeyGetMouseButtonString
+LogiGkeyInit
+LogiGkeyInitWithoutCallback
+LogiGkeyInitWithoutContext
+LogiGkeyIsKeyboardGkeyPressed
+LogiGkeyIsMouseButtonPressed
+LogiGkeyShutdown
+
+     */
+
+
+    [DllImport("LogitechGkeyEnginesWrapper.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern int LogiGkeyIsKeyboardGkeyPressed(int gKeyNumber, int modeNumber);
+
+
+
+    [DllImport("LogitechGkeyEnginesWrapper.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern void LogiGkeyShutdown();
+
+
+    [DllImport("LogitechGkeyEnginesWrapper.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern int LogiGkeyInitWithoutCallback();
+
+
+    [DllImport("LogitechGkeyEnginesWrapper.dll", CharSet = CharSet.Auto, SetLastError = true)]
+    public static extern IntPtr LogiGkeyGetKeyboardGkeyString(int gKeyNumber, int modeNumber);
+
+
+    
+}
---
+++ Logitech/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace Logitech {
+    internal class Program {
+        private static volatile bool isRunning = true;
+        static void Main(string[] args) {
+            try {
+                InterceptKeys.Start();
+                int r = LogitechGKeys.LogiGkeyInitWithoutCallback();
+                Console.WriteLine($"Logitech Init result: {r}");
+
+                
+                Thread t = new Thread(() => {
+                    while (isRunning) {
+                        Thread.Sleep(1);
+
+                        int lastKey = -1;
+                        int lastState = -1;
+                        for (int key = 1; key <= 29; key++) {
+                            for (int state = 1; state <= 3; state++) {
+                                // Store the state of G-keys, so that we can filter duplicate clicks.
+                                // Also store the timestamp.. if the key is being held down, we might want to trigger multiples? or do we?
+                                if (LogitechGKeys.LogiGkeyIsKeyboardGkeyPressed(key, state) != 0) {
+                                    Console.WriteLine($"G{key} M{state}");
+                                    lastKey = key;
+                                    lastState = state;
+                                }
+                                else {
+                                    lastKey = -1;
+                                    lastState = -1;
+
+                                }
+                            }
+
+                        }
+                    }
+                });
+
+                t.Start();
+                Application.Run();
+
+                isRunning = false;
+            }
+            finally {
+                LogitechGKeys.LogiGkeyShutdown();
+                InterceptKeys.Finalize();
+            }
+        }
+    }
+}
