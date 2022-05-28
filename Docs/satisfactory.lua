8305ac6c57282f387cb375d2047367529fac0133
m <marius00-gh@evilsoft.net>
2022-05-28 10:28:14 -0300

    Readme improvements

---
+++ Docs/satisfactory.lua
@@ -0,0 +1,65 @@
+SetBacklightColor('W', 0, 100, 0)
+SetBacklightColor('S', 0, 100, 0)
+SetBacklightColor('G3', 0, 0, 0)
+SetBacklightColor('G4', 0, 0, 0)
+
+-- Simple script to spam "e" on G4 (loot plants in Satisfactory)
+-- And autorun on G3. Autorun will cancel by clicking "S" or G3 again.
+-- G1 cancels both scripts
+
+spamKey = false
+key = 'e' -- Loot plants
+autorun = false
+
+function reset()
+	spamKey = false
+	SetBacklightColor('G4', 0, 0, 0)
+	stopAutorun()
+end
+
+function stopAutorun()
+	autorun = false
+	SetBacklightColor('G3', 0, 0, 0)
+	KeyUp('W')
+end
+
+function toggleAutorun()
+	autorun = not autorun
+	if autorun then
+		SetBacklightColor(arg, 100, 0, 0)
+		KeyDown('W')
+	else
+		stopAutorun()
+	end
+end
+
+function toggleSpamKey(keyToSpam)
+	spamKey = not spamKey
+	key = keyToSpam
+	if spamKey then
+		SetBacklightColor(arg, 0, 100, 0)
+	else
+		SetBacklightColor(arg, 0, 0, 0)
+	end
+end
+
+function OnEvent(event, arg, modifiers)
+	if event == TickEvent then
+		if spamKey then
+			KeyPress(key)
+		end
+	elseif event == InputEvent then
+		if arg == 'G4' then
+			toggleSpamKey('e')
+		elseif arg == 'G3' then
+			toggleAutorun()
+		elseif arg == 'G1' then
+			reset()
+		elseif arg == 'S' and autorun then
+			stopAutorun()
+		end
+	else
+		OutputLogMessage('Unknown event type: {0}', event)		
+	end
+end
+
